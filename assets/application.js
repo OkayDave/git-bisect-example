1d17b4d3f688d436529b535a5ac9a4acc02cf056
Dave <dave.kerr@gmail.com>
2018-11-04 04:47:24 +0000

    Wrap first letters in span

--- assets/application.js
+++ assets/application.js
@@ -1,3 +1,22 @@
 function fancyBlockQuotes() {
-  
+  formatFirstLetters();
 }
+
+function formatFirstLetters() {
+  window.letters = wrapFirstLetters();
+}
+
+function wrapFirstLetters() {
+  letters = [];
+  $('p.quote').each(function(i) {
+    var chars = $(this).text().split('');
+    letters << chars[0];
+    chars[0] = "<span class='first-char'>" + chars[0] + "</span>";
+    $(this).html(chars.join(''));
+  });
+  return letters;
+}
+
+$(document).ready(function() {
+  fancyBlockQuotes();
+})
